484eb168c02af9e27b498073e8a735ab117817f0
Alfonso Landeros <alandero@ucr.edu>
2025-05-30 10:06:23 -0700

    Refactor

--- src/leastsquares.jl
+++ src/leastsquares.jl
@@ -2,41 +2,31 @@
 ### Initialization
 ###
 
-function initblocks!(::Type{T}, d, x, g, linmap, b, n_blk, lambda, use_qlb, tol_powm) where T
-  A, AtA = linmap.A, linmap.AtA
+function initblocks!(::Type{T}, AtApI::GramPlusDiag, lambda, use_qlb, n_blk) where T
+  A, AtA = AtApI.A, AtApI.AtA
   n_obs, n_var = size(A)
   var_per_blk = cld(n_var, n_blk)
   # Compute blocks along diagonal, Dₖ = Dₖₖ = AₖᵀAₖ + λI and extract their Cholesky decompositions
   if use_qlb
     if size(AtA, 1) > 0
-      D = BlkDiagHessian(linmap, n_blk; alpha=1, beta=0, factor=false)
+      D = BlkDiagHessian(AtApI, n_blk; alpha=1, beta=0, factor=false)
     else
       D = BlkDiagHessian(A, n_blk; alpha=1, beta=0, factor=false, gram=n_obs >= var_per_blk)
     end
-    lambda_max = estimate_dominant_eigval(GramPlusDiag(linmap, 1, 0), D, maxiter=3)
+    lambda_max = estimate_dominant_eigval(GramPlusDiag(AtApI, 1, 0), D, maxiter=3)
     D = update_factors!(D, 1, lambda + lambda_max)
   else
     if size(AtA, 1) > 0
-      D = BlkDiagHessian(linmap, n_blk; alpha=n_blk, beta=lambda, factor=true)
+      D = BlkDiagHessian(AtApI, n_blk; alpha=n_blk, beta=lambda, factor=true)
     else
       D = BlkDiagHessian(A, n_blk; alpha=n_blk, beta=lambda, factor=true, gram=n_obs >= var_per_blk)
     end
   end
-
-  # Initialize the difference, d₁ = x₁ - x₀
-  r = copy(b)
-  mul!(r, A, x, -one(T), one(T))  # r = b - A⋅x
-  mul!(g, transpose(A), r)        # -∇₀ = Aᵀ⋅r - λx
-  !iszero(lambda) && axpy!(-T(lambda), x, g)
-
-  ldiv!(d, D, g) # d₁ = D⁻¹(-∇₀)
-  @. x = x + d   # x₁ = x₀ + d₁
-
-  return r, D
+  return D
 end
 
-function initdiag!(::Type{T}, d, x, g, linmap, b, lambda, use_qlb, tol_powm) where T
-  A, AtA = linmap.A, linmap.AtA
+function initdiag!(::Type{T}, AtApI::GramPlusDiag, lambda, use_qlb) where T
+  A, AtA = AtApI.A, AtApI.AtA
   if size(AtA, 1) == 0
     diag = zeros(T, size(A, 2))
     for k in axes(A, 2)
@@ -48,11 +38,19 @@ function initdiag!(::Type{T}, d, x, g, linmap, b, lambda, use_qlb, tol_powm) whe
   D = Diagonal(diag)
 
   if use_qlb
-    lambda_max = estimate_dominant_eigval(GramPlusDiag(linmap, 1, 0), D, maxiter=3)
+    lambda_max = estimate_dominant_eigval(GramPlusDiag(AtApI, 1, 0), D, maxiter=3)
     @. D.diag += lambda + lambda_max
+  else
+    n_blk = length(D.diag)
+    @. D.diag = n_blk * D.diag
   end
+  return D
+end
 
+function init_recurrences!(d, x, g, w, AtApI::GramPlusDiag, b, D, lambda)
   # Initialize the difference, d₁ = x₁ - x₀
+  T = eltype(AtApI)
+  A = AtApI.A
   r = copy(b)
   mul!(r, A, x, -one(T), one(T))  # r = b - A⋅x
   mul!(g, transpose(A), r)        # -∇₀ = Aᵀ⋅r - λx
@@ -61,7 +59,11 @@ function initdiag!(::Type{T}, d, x, g, linmap, b, lambda, use_qlb, tol_powm) whe
   ldiv!(d, D, g) # d₁ = D⁻¹(-∇₀)
   @. x = x + d   # x₁ = x₀ + d₁
 
-  return r, D
+  # Current negative gradient, -∇₁
+  mul!(w, AtApI, d) # (AᵀA+λI) d₁
+  @. g = g - w      # -∇₁ = -∇₀ - (AᵀA+λI) d₁
+
+  return r
 end
 
 ###
@@ -70,6 +72,8 @@ end
 
 function solve_OLS(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_blk::Int;
   lambda::Float64 = 0.0,
+  use_qlb::Bool = false,
+  gram::Bool = _cache_gram_heuristic_(A),
   kwargs...
 ) where T
   #
@@ -80,35 +84,31 @@ function solve_OLS(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_blk::Int
   @assert var_per_blk > 0
   @assert lambda >= 0
 
+  AtApI = GramPlusDiag(A; alpha=one(T), beta=T(lambda), gram=gram)
+
   if var_per_blk > 1
-    _solve_OLS_blkdiag(A, b, x0, n_blk; lambda=lambda, kwargs...)
+    D_blkd = initblocks!(T, AtApI, lambda, use_qlb, n_blk)
+    _solve_OLS_loop(AtApI, D_blkd, b, x0, T(lambda); kwargs...)
   else
-    _solve_OLS_diag(A, b, x0, n_blk; lambda=lambda, kwargs...)
+    D_diag = initdiag!(T, AtApI, lambda, use_qlb)
+    _solve_OLS_loop(AtApI, D_diag, b, x0, T(lambda); kwargs...)
   end
 end
 
-function _solve_OLS_blkdiag(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_blk::Int;
-  lambda::Float64 = 0.0,
+function _solve_OLS_loop(AtApI::GramPlusDiag{T}, D, b::Vector{T}, x0::Vector{T}, lambda::T;
   maxiter::Int = 100,
   gtol::Float64 = 1e-3,
-  use_qlb::Bool = false,
-  tol_powm::Float64 = T(minimum(size(A))),
-  gram::Bool = _cache_gram_heuristic_(A),
 ) where T
   #
-  n_obs, n_var = size(A)
+  A = AtApI.A
+  n_var = size(A, 2)
 
   # Main matrices and vectors
   x = deepcopy(x0)
   d = zeros(n_var)
   g = zeros(n_var)
-  tmp = zeros(n_var)
-  AtApI = GramPlusDiag(A; alpha=one(T), beta=T(lambda), gram=gram)
-  r, D = initblocks!(T, d, x, g, AtApI, b, n_blk, lambda, use_qlb, tol_powm)
-
-  # Current negative gradient, -∇₁
-  mul!(tmp, AtApI, d) # (AᵀA+λI) d₁
-  @. g = g - tmp      # -∇₁ = -∇₀ - (AᵀA+λI) d₁
+  w = zeros(n_var)
+  r = init_recurrences!(d, x, g, w, AtApI, b, D, lambda)
 
   # Iterate the algorithm map
   iter = 1
@@ -118,73 +118,15 @@ function _solve_OLS_blkdiag(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n
     iter += 1
 
     # Update difference dₙ₊₁ = [I - D⁻¹(AᵀA+λI)] dₙ
-    ldiv!(D, tmp)
-    @. d = d - tmp
+    ldiv!(D, w)
+    @. d = d - w
 
     # Update coefficients
     @. x = x + d
 
     # Compute (AᵀA+λI) dₙ₊₁ and -∇ₙ₊₁
-    mul!(tmp, AtApI, d)
-    @. g = g - tmp      # assumes g is always -∇
-    converged = norm(g) <= gtol
-  end
-
-  # final residual
-  copyto!(r, b)
-  mul!(r, A, x, -1.0, 1.0)
-
-  stats = (
-    iterations = iter,
-    converged = converged,
-    xnorm = norm(x),
-    rnorm = norm(r),
-    gnorm = norm(g),
-  )
-
-  return x, r, stats
-end
-
-function _solve_OLS_diag(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_blk::Int;
-  lambda::Float64 = 0.0,
-  maxiter::Int = 100,
-  gtol::Float64 = 1e-3,
-  use_qlb::Bool = false,
-  tol_powm::Float64 = T(minimum(size(A))),
-  gram::Bool = _cache_gram_heuristic_(A),
-) where T
-  #
-  n_obs, n_var = size(A)
-
-  # Main matrices and vectors
-  x = deepcopy(x0)
-  d = zeros(n_var)
-  g = zeros(n_var)
-  tmp = zeros(n_var)
-  AtApI = GramPlusDiag(A; alpha=one(T), beta=T(lambda), gram=gram)
-  r, D = initdiag!(T, d, x, g, AtApI, b, lambda, use_qlb, tol_powm)
-
-  # Current negative gradient, -∇₁
-  mul!(tmp, AtApI, d) # (AᵀA+λI) d₁
-  @. g = g - tmp      # -∇₁ = -∇₀ - (AᵀA+λI) d₁
-
-  # Iterate the algorithm map
-  iter = 1
-  converged = norm(g) <= gtol
-
-  while !converged && (iter < maxiter)
-    iter += 1
-
-    # Update difference dₙ₊₁ = [I - D⁻¹(AᵀA+λI)] dₙ
-    ldiv!(D, tmp)
-    @. d = d - tmp
-
-    # Update coefficients
-    @. x = x + d
-
-    # Compute (AᵀA+λI) dₙ₊₁ and -∇ₙ₊₁
-    mul!(tmp, AtApI, d)
-    @. g = g - tmp      # assumes g is always -∇
+    mul!(w, AtApI, d)
+    @. g = g - w      # assumes g is always -∇
     converged = norm(g) <= gtol
   end
 
