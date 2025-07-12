e5e7042738b6d90167a59b494c45241ae19f11bb
Thing 1 <nobdyhopeitsnotreal@ucr.fake.edu>
2025-07-12 15:15:18 -0700

    Consolidate with Nesterov accel

--- src/quantilereg.jl
+++ src/quantilereg.jl
@@ -27,7 +27,7 @@ end
 
 function qreg_objective(r, q)
   let r = r, q = q
-    f(r) = qreg_loss(r, q)
+    f(ri) = qreg_loss(ri, q)
     mapreduce(f, +, r) / length(r)
   end
 end
@@ -39,10 +39,36 @@ function qreg_loss_uniform(r::T, q::T, h::T) where T <: Real
   return (q-1//2)*r + 1//2*C
 end
 
+weight_uniform(ri, h) = ifelse(abs(ri) > h, sign(ri), ri)
+weight_uniform(ri, q, h) = (q-1//2) + weight_uniform(ri, h)
+
 function qreg_objective_uniform(r, q, h)
   let r =r, q = q, h = h
-    f(r) = qreg_loss_uniform(r, q, h)
+    f(ri) = qreg_loss_uniform(ri, q, h)
     mapreduce(f, +, r) / length(r)
+  end
+end
+
+# sharp approximation
+abs_approx(x, ϵ) = sqrt(x*x + ϵ)
+weight_sharp(ri, ϵ) = 1//2*ri/abs_approx(ri, ϵ)
+weight_sharp(ri, q, ϵ) = (q-1//2) + weight_sharp(ri, ϵ)
+
+function qreg_loss_sharp(r::T, q::T, ϵ::T) where T <: Real
+  return (q-1//2)*r + 1//2*abs_approx(r, ϵ)
+end
+
+function qreg_objective_sharp(r, q, ϵ)
+  let r = r, q = q, ϵ = ϵ
+    f(ri) = qreg_loss_sharp(ri, q, ϵ)
+    mapreduce(f, +, r) / length(r)
+  end
+end
+
+function compute_weights!(weightf, z, r, q, c)
+  let r = r, q = q, c = c
+    f(ri) = weightf(ri, q, c)
+    map!(f, z, r)
   end
 end
 
@@ -64,7 +90,7 @@ function __QREG_loop__(workspace, linmaps, q, h, rtol, gtol, maxiter)
   # Iterate the algorithm map
   iter = 0  # outer iterations; Moreau majorization
   inner = 0 # inner iterations; QUB majorization
-  k = 0     # Nesterov iterations
+  k = 1     # Nesterov iterations
   converged = false
   f_curr = qreg_objective_uniform(r, q, h)
   while !converged && (iter < maxiter)
@@ -78,27 +104,32 @@ function __QREG_loop__(workspace, linmaps, q, h, rtol, gtol, maxiter)
     # Solve with QUB
     #
     init_recurrences!((x, g, d, w), AtApI, u, H)
-    inner_iter, is_stationary = __OLS_loop__((x, g, d, w), (AtApI, H), gtol, maxiter, 1)
-    # @show iter, inner_iter, is_stationary
+    inner_iter, _ = __OLS_loop__((x, g, d, w), (AtApI, H), gtol, maxiter, 1)
     inner += inner_iter
-    if inner_iter > 0
+    if false
       #
       # Nesterov acceleration
       #
-      k += 1
-      @. x_next = x
-      @. x = k/(k+3) * (x_next - x_prev); @. x = x + x_next
-      @. x_prev = x_next
+      if k > 1
+        @. x_next = x
+        @. x = (x_next - x_prev)
+        @. x_prev = x_next
+        @. x = x_next + (k-1)/(k+2)*x
+      else
+        @. x_prev = x
+      end
+      @. r = b
+      mul!(r, A, x, -one(T), one(T))
+      f_prev = f_curr
+      f_curr = qreg_objective_uniform(r, q, h)
+      k = ifelse(f_curr > f_prev, 1, k+1)
+    else
+      @. r = b
+      mul!(r, A, x, -one(T), one(T))
+      f_prev = f_curr
+      f_curr = qreg_objective_uniform(r, q, h)
     end
-    #
-    # Update residual
-    #
-    @. r = b
-    mul!(r, A, x, -one(T), one(T))
     iter += 1
-    f_prev = f_curr
-    f_curr = qreg_objective_uniform(r, q, h)
-    if f_curr > f_prev k = 0 end
     converged = abs(f_curr - f_prev) < rtol * (f_prev + 1)
   end
 
@@ -178,6 +209,8 @@ function solve_QREG_lbfgs(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_b
   gtol::Float64   = 1e-3,
   rtol::Float64   = 1e-6,
   version::Int    = 1,
+  epsilon::Real   = h*h/4,
+  accel::Bool     = false,
 ) where T
   #
   n_obs, n_var = size(A)
@@ -202,6 +235,12 @@ function solve_QREG_lbfgs(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_b
   cache = LBFGSCache{T}(n_var, memory)
   workspace = (x, x_next, x_prev, g, d, r, u, z, cache)
   run = let
+    wfun1(dst, src) = compute_weights!(weight_uniform, dst, src, q, h)
+    ofun1(r) = qreg_objective_uniform(r, q, h)
+    
+    wfun2(dst, src) = compute_weights!(weight_sharp, dst, src, q, epsilon)
+    ofun2(r) = qreg_objective_sharp(r, q, epsilon)
+
     function(H)
       # constants/linear maps
       linmaps = (A, b, AtApI, H)
@@ -212,9 +251,40 @@ function solve_QREG_lbfgs(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_b
       @. x_prev = x
       # Solve the QREG problem
       if version == 1
-        _, _iter, _inner, _converged = __QREG_lbfgs__(workspace, linmaps, q, h, rtol, gtol, maxiter)
+        # double loop
+        _, _iter, _inner, _converged = __QREG_lbfgs__(workspace, linmaps, q, h, rtol, gtol, maxiter, accel)
       elseif version == 2
-        _, _iter, _inner, _converged = __QREG_lbfgs_single__(workspace, linmaps, q, h, rtol, maxiter)
+        #
+        # L-BFGS on kernel-smoothed objective
+        #
+        _, _iter, _inner, _converged = __QREG_lbfgs_single__(wfun1, ofun1, workspace, linmaps, rtol, maxiter, accel)
+      elseif version == 3
+        #
+        # L-BFGS on kernel-smoothed objective; re-calculate QUB matrix
+        #
+        @. r = b
+        mul!(r, A, x, -one(T), one(T))
+        w = map(ri -> weight_uniform(ri, h), r)
+        AtWA = A'*Diagonal(w)*A
+        rho, _ = powm!(AtA - AtWA, ones(T, n_var), maxiter=3)
+        J = Diagonal(AtWA) + abs(rho)*I
+        _, _iter, _inner, _converged = __QREG_lbfgs_single__(wfun1, ofun1, workspace, (A, b, AtApI, J), rtol, maxiter, accel)
+      elseif version == 4
+        #
+        # L-BFGS on sharp quadratic approximation
+        #
+        _, _iter, _inner, _converged = __QREG_lbfgs_single__(wfun2, ofun2, workspace, linmaps, rtol, maxiter, accel)
+      elseif version == 5
+        #
+        # L-BFGS on sharp quadratic approximation; re-calculate QUB matrix
+        #
+        @. r = b
+        mul!(r, A, x, -one(T), one(T))
+        w = map(ri -> weight_sharp(ri, epsilon), r)
+        AtWA = A'*Diagonal(w)*A
+        rho, _ = powm!(AtA - AtWA, ones(T, n_var), maxiter=3)
+        J = Diagonal(AtWA) + abs(rho)*I
+        _, _iter, _inner, _converged = __QREG_lbfgs_single__(wfun2, ofun2, workspace, (A, b, AtApI, J), rtol, maxiter, accel)
       end
       _inner += iter0
       return _iter, _inner, _converged
@@ -228,14 +298,15 @@ function solve_QREG_lbfgs(A::AbstractMatrix{T}, b::Vector{T}, x0::Vector{T}, n_b
     converged = converged,
     xnorm = norm(x),
     rnorm = norm(r),
-    loss1 = qreg_objective(r, q),             # check function loss
-    loss2 = qreg_objective_uniform(r, q, h),  # smoothed loss
+    loss1 = qreg_objective(r, q),               # check function loss
+    loss2 = qreg_objective_uniform(r, q, h),    # smoothed loss
+    loss3 = qreg_objective_sharp(r, q, epsilon) # sharp approximation
   )
 
   return x, r, stats
 end
 
-function __QREG_lbfgs__(workspace, linmaps, q, h, rtol, gtol, maxiter)
+function __QREG_lbfgs__(workspace, linmaps, q, h, rtol, gtol, maxiter, accel)
   # unpack
   x, x_next, x_prev, g, d, r, u, z, cache = workspace
   A, b, AtApI, H = linmaps
@@ -264,39 +335,41 @@ function __QREG_lbfgs__(workspace, linmaps, q, h, rtol, gtol, maxiter)
     #
     cache.current_index = 0
     cache.current_size = 0
-    inner_iter, is_stationary = __OLS_lbfgs__((x, g, d, w, cache), (A, u, AtApI, H), gtol, maxiter, 0)
-    # @show iter, inner_iter, is_stationary
+    inner_iter, _ = __OLS_lbfgs__((x, g, d, w, cache), (A, u, AtApI, H), gtol, maxiter, 0)
     inner += inner_iter
-    if inner_iter > 0
+    if accel
       #
       # Nesterov acceleration
       #
-      k += 1
-      @. x_next = x
-      @. x = k/(k+3) * (x_next - x_prev); @. x = x + x_next
-      @. x_prev = x_next
+      if k > 1
+        @. x_next = x
+        @. x = (x_next - x_prev)
+        @. x_prev = x_next
+        @. x = x_next + (k-1)/(k+2)*x
+      else
+        @. x_prev = x
+      end
+      @. r = b
+      mul!(r, A, x, -one(T), one(T))
+      f_prev = f_curr
+      f_curr = qreg_objective_uniform(r, q, h)
+      k = ifelse(f_curr > f_prev, 1, k+1)
+    else
+      @. r = b
+      mul!(r, A, x, -one(T), one(T))
+      f_prev = f_curr
+      f_curr = qreg_objective_uniform(r, q, h)
     end
-    #
-    # Update residual
-    #
-    @. r = b
-    mul!(r, A, x, -one(T), one(T))
     iter += 1
-    f_prev = f_curr
-    f_curr = qreg_objective_uniform(r, q, h)
-    if f_curr > f_prev k = 0 end
     converged = abs(f_curr - f_prev) < rtol * (f_prev + 1)
   end
 
   return r, iter, inner, converged
 end
 
-function __QREG_lbfgs_single__(workspace, linmaps, q, h, rtol, maxiter)
-  # gradient calculation
-  weights(r, q, h) = q - 1/2 + ifelse(abs(r) > h, sign(r), r)
-
+function __QREG_lbfgs_single__(compute_weights!, objective, workspace, linmaps, rtol, maxiter, accel)
   # unpack
-  x, _, _, g, d, r, u, z, cache = workspace
+  x, x_next, x_prev, g, d, r, u, z, cache = workspace
   A, b, _, H = linmaps
   T = eltype(A)
 
@@ -310,16 +383,14 @@ function __QREG_lbfgs_single__(workspace, linmaps, q, h, rtol, maxiter)
 
   # Iterate the algorithm map
   iter = 0
+  k = 1     # Nesterov iterations
   converged = false
-  f_curr = qreg_objective_uniform(r, q, h)
+  f_curr = objective(r)
   alpha = one(T)
   while !converged && (iter < maxiter)
-    # Compute gradient
-    let z = z, r = r, q = q, h = h
-      w(ri) = weights(ri, q, h)
-      map!(w, z, r)
-    end
-    mul!(g, transpose(A), z) # negative gradient
+    # Compute negative gradient
+    compute_weights!(z, r)
+    mul!(g, transpose(A), z)
 
     # Update the LBFGS workspace and compute the next direction
     iter > 1 && update!(cache, alpha, d, g)
@@ -335,15 +406,36 @@ function __QREG_lbfgs_single__(workspace, linmaps, q, h, rtol, maxiter)
     alpha = one(T)
     @. u = r - alpha*z
     f_prev = f_curr
-    f_curr = qreg_objective_uniform(u, q, h)
+    f_curr = objective(u)
     while f_curr > f_prev
       alpha = 1//2 * alpha
       @. u = r - alpha*z
-      f_curr = qreg_objective_uniform(u, q, h)
+      f_curr = objective(u)
     end
     @. x = x + alpha*d
-    @. r = u
+    if accel
+      #
+      # Nesterov acceleration
+      #
+      if k > 1
+        @. x_next = x
+        @. x = (x_next - x_prev)
+        @. x_prev = x_next
+        @. x = x_next + (k-1)/(k+2)*x
 
+        @. r = b
+        mul!(r, A, x, -one(T), one(T))
+
+        f_prev = f_curr
+        f_curr = objective(r)
+      else
+        @. x_prev = x
+        @. r = u
+      end
+      k = ifelse(f_curr > f_prev, 1, k+1)
+    else
+      @. r = u
+    end
     iter += 1
     converged = abs(f_curr - f_prev) < rtol * (f_prev + 1)
   end
