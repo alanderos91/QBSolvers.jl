c8e41732bf31d0d20151fba430d2e073379b4760
Alfonso Landeros <alandero@ucr.edu>
2025-09-19 00:21:51 -0700

    Tidy up unused ideas

--- benchmark/linearmaps.jl
+++ benchmark/linearmaps.jl
@@ -17,7 +17,6 @@ x0 = zeros(p)
 AtA = A'A
 w, v= similar(AtA, p), fill!(similar(AtA, p), 1)
 
-
 @info "[GramPlusDiag] Construction"
 @benchmark( GramPlusDiag($A, alpha=1, beta=0) ) |> display
 
@@ -28,39 +27,3 @@ w, v= similar(AtA, p), fill!(similar(AtA, p), 1)
 AtAm = GramPlusDiag(A, alpha=1, beta=0);
 @benchmark( mul!($w, $AtAm, $v) ) |> display
 
-# BlkDiagHessian
-n_blk = 256
-
-@info "[BlkDiagHessian] Construction, factor=false"
-@benchmark( BlkDiagHessian($A, $n_blk, alpha=1, beta=0, factor=false) ) |> display
-
-@info "[BlkDiagHessian] Construction, factor=true"
-@benchmark( BlkDiagHessian($A, $n_blk, alpha=1, beta=0, factor=true) ) |> display
-
-Dm = BlkDiagHessian(A, n_blk, alpha=1, beta=0, factor=true);
-D = convert(Matrix, Dm);
-
-@info "[BlkDiagHessian] w = D*v, Matrix"
-@benchmark( mul!($w, $D, $v) ) |> display
-
-@info "[BlkDiagHessian] w = D*v, LinearMap"
-@benchmark( mul!($w, $Dm, $v) ) |> display
-
-@benchmark( ldiv!($w, $Dm, $v) ) |> display
-
-# Gram - BlkDiag
-
-@info "[GramMinusBlkDiag] Construction, full"
-
-@benchmark( begin
-  local D = BlkDiagHessian($A, $n_blk, alpha=1, beta=0, factor=false)
-  GramMinusBlkDiag($AtAm, D)
-end) |> display
-
-@info "GramMinusBlkDiag] Construction, object only"
-@benchmark( GramMinusBlkDiag($AtAm, $Dm) ) |> display
-
-Mm = GramMinusBlkDiag(AtAm, Dm);
-M = convert(Matrix, Mm);
-@benchmark( mul!($w, $M, $v) ) |> display
-@benchmark( mul!($w, $Mm, $v) ) |> display
